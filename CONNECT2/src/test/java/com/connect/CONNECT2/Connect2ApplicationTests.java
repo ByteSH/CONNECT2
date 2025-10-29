527f26e5ce1c270a177b91ddc9778741b895caab
ByteSH <hamzash.byte@gmail.com>
2025-10-30 02:27:14 +0530

    Initializing Spring boot project
    And Basic configuration of application.yml(including MySQL & Logging config), logback.xml, MySQL, Lombok.

---
+++ CONNECT2/src/test/java/com/connect/CONNECT2/Connect2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect.CONNECT2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Connect2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
